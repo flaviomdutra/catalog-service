9b119282900e1ce2d6de70983c2a1f94fc18d60b
Flávio Dutra <flaviomdutra@outlook.com>
2024-05-04 19:10:55 -0300

    feat: Add new classes for error handling and validation
    The code changes include the addition of several new classes related to error handling and validation. These classes provide functionality for handling and reporting errors, as well as validating data. This commit adds the following classes:
    
    - `Error`: A record class representing an error message.
    - `NotificationException`: An exception class that extends `DomainException` and includes a `Notification` object for error reporting.
    - `Validator`: An abstract class for implementing data validation logic.
    - `InstantUtils`: A utility class for working with `Instant` objects and timestamps.
    - `ValidationHandler`: An interface for handling validation errors and aggregating them.
    
    These changes lay the foundation for improved error handling and validation in the project.

---
+++ domain/src/main/java/com/fullcycle/catalogo/domain/category/Category.java
@@ -0,0 +1,106 @@
+package com.fullcycle.catalogo.domain.category;
+
+import com.fullcycle.catalogo.domain.validation.Error;
+import com.fullcycle.catalogo.domain.validation.ValidationHandler;
+
+import java.time.Instant;
+
+public class Category {
+    private final String id;
+    private final String name;
+    private final String description;
+    private final boolean active;
+    private final Instant createdAt;
+    private final Instant updatedAt;
+    private final Instant deletedAt;
+
+    private Category(
+            final String anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant aCreationDate,
+            final Instant aUpdateDate,
+            final Instant aDeleteDate
+    ) {
+        this.id = anId;
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = aCreationDate;
+        this.updatedAt = aUpdateDate;
+        this.deletedAt = aDeleteDate;
+    }
+
+    public static Category with(
+            final String anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant aCreationDate,
+            final Instant aUpdateDate,
+            final Instant aDeleteDate
+    ) {
+        return new Category(
+                anId,
+                aName,
+                aDescription,
+                isActive,
+                aCreationDate,
+                aUpdateDate,
+                aDeleteDate
+        );
+    }
+
+    public static Category with(final Category category) {
+        return with(
+                category.id(),
+                category.name(),
+                category.description(),
+                category.active(),
+                category.createdAt(),
+                category.updatedAt(),
+                category.deletedAt()
+        );
+    }
+
+    public Category validate(final ValidationHandler aHandler) {
+        if (id == null || id.isBlank()) {
+            aHandler.append(new Error("'id' should not be empty"));
+        }
+
+        if (name == null || name.isBlank()) {
+            aHandler.append(new Error("'name' should not be empty"));
+        }
+
+        return this;
+    }
+
+    public String id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public boolean active() {
+        return active;
+    }
+
+    public Instant createdAt() {
+        return createdAt;
+    }
+
+    public Instant updatedAt() {
+        return updatedAt;
+    }
+
+    public Instant deletedAt() {
+        return deletedAt;
+    }
+}
