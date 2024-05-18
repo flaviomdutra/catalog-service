399fee312a8c62dc049febbf014986575356b34a
Flávio Dutra <flaviomdutra@outlook.com>
2024-05-18 15:26:25 -0300

    feat: Add Kafka and Zookeeper Docker Compose configuration

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -3,7 +3,9 @@ docker network create elastic
 
 # Criar os docker volumes
 docker volume create es01
+docker volume create kafka01
 
 # Criar as pastas com permissões
 docker compose -f elk/docker-compose.yml up -d elasticsearch
+docker compose -f kafka/docker-compose.yml up -d
 
