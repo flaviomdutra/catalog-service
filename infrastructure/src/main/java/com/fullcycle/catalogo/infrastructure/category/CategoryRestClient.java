c46a676d9b95eea91b15e003b0d33320d81cfccf
Flávio Dutra <flaviomdutra@outlook.com>
2024-05-28 21:03:06 -0300

    feat: Update read timeout for categories in CategoryRestClient

--- infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/category/CategoryRestClient.java
+++ infrastructure/src/main/java/com/fullcycle/catalogo/infrastructure/category/CategoryRestClient.java
@@ -4,6 +4,11 @@ import com.fullcycle.catalogo.domain.category.Category;
 import com.fullcycle.catalogo.infrastructure.category.models.CategoryDTO;
 import com.fullcycle.catalogo.infrastructure.configuration.annotations.Categories;
 import com.fullcycle.catalogo.infrastructure.utils.HttpClient;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -11,6 +16,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 @Component
+@CacheConfig(cacheNames = "admin-categories")
 public class CategoryRestClient implements CategoryClient, HttpClient {
 
     public static final String NAMESPACE = "categories";
@@ -26,6 +32,10 @@ public class CategoryRestClient implements CategoryClient, HttpClient {
         return NAMESPACE;
     }
 
+    @Cacheable(key = "#categoryId")
+    @Bulkhead(name = NAMESPACE)
+    @CircuitBreaker(name = NAMESPACE)
+    @Retry(name = NAMESPACE)
     public Optional<Category> categoryOfId(String categoryId) {
         return doGet(categoryId, () ->
             this.restClient.get()
--- infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/category/CategoryRestClientTest.java
+++ infrastructure/src/test/java/com/fullcycle/catalogo/infrastructure/category/CategoryRestClientTest.java
@@ -4,6 +4,9 @@ import com.fullcycle.catalogo.AbstractRestClientTest;
 import com.fullcycle.catalogo.domain.Fixture;
 import com.fullcycle.catalogo.domain.exceptions.InternalErrorException;
 import com.fullcycle.catalogo.infrastructure.category.models.CategoryDTO;
+import io.github.resilience4j.bulkhead.BulkheadFullException;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,6 +62,50 @@ public class CategoryRestClientTest extends AbstractRestClientTest {
         verify(1, getRequestedFor(urlPathEqualTo("/api/categories/%s".formatted(aulas.id()))));
     }
 
+    @Test
+    public void givenACategory_whenReceiveTwoCalls_shouldReturnCachedValue() {
+        // given
+        final var aulas = Fixture.Categories.aulas();
+
+        final var responseBody = writeValueAsString(new CategoryDTO(
+            aulas.id(),
+            aulas.name(),
+            aulas.description(),
+            aulas.active(),
+            aulas.createdAt(),
+            aulas.updatedAt(),
+            aulas.deletedAt()
+        ));
+
+        stubFor(
+            get(urlPathEqualTo("/api/categories/%s".formatted(aulas.id())))
+                .willReturn(aResponse()
+                    .withStatus(200)
+                    .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .withBody(responseBody)
+                )
+        );
+
+        // when
+        target.categoryOfId(aulas.id()).get();
+        target.categoryOfId(aulas.id()).get();
+        final var actualCategory = target.categoryOfId(aulas.id()).get();
+
+        // then
+        Assertions.assertEquals(aulas.id(), actualCategory.id());
+        Assertions.assertEquals(aulas.name(), actualCategory.name());
+        Assertions.assertEquals(aulas.description(), actualCategory.description());
+        Assertions.assertEquals(aulas.active(), actualCategory.active());
+        Assertions.assertEquals(aulas.createdAt(), actualCategory.createdAt());
+        Assertions.assertEquals(aulas.updatedAt(), actualCategory.updatedAt());
+        Assertions.assertEquals(aulas.deletedAt(), actualCategory.deletedAt());
+
+        final var actualCachedValue = cache("admin-categories").get(aulas.id());
+        Assertions.assertEquals(actualCategory, actualCachedValue.get());
+
+        verify(1, getRequestedFor(urlPathEqualTo("/api/categories/%s".formatted(aulas.id()))));
+    }
+
     // 5XX
     @Test
     public void givenACategory_whenReceive5xxFromServer_shouldReturnInternalError() {
@@ -82,6 +129,8 @@ public class CategoryRestClientTest extends AbstractRestClientTest {
 
         // then
         Assertions.assertEquals(expectedErrorMessage, actualEx.getMessage());
+
+        verify(2, getRequestedFor(urlPathEqualTo("/api/categories/%s".formatted(expectedId))));
     }
 
     // 404
@@ -131,7 +180,7 @@ public class CategoryRestClientTest extends AbstractRestClientTest {
                 .willReturn(aResponse()
                     .withStatus(200)
                     .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                    .withFixedDelay(1600)
+                    .withFixedDelay(600)
                     .withBody(responseBody)
                 )
         );
@@ -141,5 +190,68 @@ public class CategoryRestClientTest extends AbstractRestClientTest {
 
         // then
         Assertions.assertEquals(expectedErrorMessage, actualEx.getMessage());
+
+        verify(2, getRequestedFor(urlPathEqualTo("/api/categories/%s".formatted(aulas.id()))));
+    }
+
+    @Test
+    public void givenACategory_whenBulkheadIsFull_shouldReturnError() {
+        // given
+        final var expectedErrorMessage = "Bulkhead 'categories' is full and does not permit further calls";
+
+        acquireBulkheadPermission(CATEGORY);
+
+        // when
+        final var actualEx = Assertions.assertThrows(BulkheadFullException.class, () -> target.categoryOfId("123"));
+
+        // then
+        Assertions.assertEquals(expectedErrorMessage, actualEx.getMessage());
+
+        releaseBulkheadPermission(CATEGORY);
+    }
+
+    @Test
+    public void givenCall_whenCBIsOpen_shouldReturnError() {
+        // given
+        transitionToOpenState(CATEGORY);
+        final var expectedId = "123";
+        final var expectedErrorMessage = "CircuitBreaker 'categories' is OPEN and does not permit further calls";
+
+        // when
+        final var actualEx = Assertions.assertThrows(CallNotPermittedException.class, () -> this.target.categoryOfId(expectedId));
+
+        // then
+        checkCircuitBreakerState(CATEGORY, CircuitBreaker.State.OPEN);
+        Assertions.assertEquals(expectedErrorMessage, actualEx.getMessage());
+
+        verify(0, getRequestedFor(urlPathEqualTo("/api/categories/%s".formatted(expectedId))));
+    }
+
+    @Test
+    public void givenServerError_whenIsMoreThanThreshold_shouldOpenCircuitBreaker() {
+        // given
+        final var expectedId = "123";
+        final var expectedErrorMessage = "CircuitBreaker 'categories' is OPEN and does not permit further calls";
+
+        final var responseBody = writeValueAsString(Map.of("message", "Internal Server Error"));
+
+        stubFor(
+            get(urlPathEqualTo("/api/categories/%s".formatted(expectedId)))
+                .willReturn(aResponse()
+                    .withStatus(500)
+                    .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .withBody(responseBody)
+                )
+        );
+
+        // when
+        Assertions.assertThrows(InternalErrorException.class, () -> this.target.categoryOfId(expectedId));
+        final var actualEx = Assertions.assertThrows(CallNotPermittedException.class, () -> this.target.categoryOfId(expectedId));
+
+        // then
+        checkCircuitBreakerState(CATEGORY, CircuitBreaker.State.OPEN);
+        Assertions.assertEquals(expectedErrorMessage, actualEx.getMessage());
+
+        verify(3, getRequestedFor(urlPathEqualTo("/api/categories/%s".formatted(expectedId))));
     }
 }
